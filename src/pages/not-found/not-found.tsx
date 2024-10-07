09e343ddb13dbf94c58c5eb81e31972d261718b0
frubes05 <filip.rubes2@gmail.com>
2024-10-07 08:04:43 +0200

    main: add design for 404 page

--- src/pages/not-found/not-found.tsx
+++ src/pages/not-found/not-found.tsx
@@ -1,7 +1,27 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { FiAlertTriangle } from "react-icons/fi";
+
 const NotFound = () => {
   return (
-    <div>not-found</div>
-  )
-}
+    <div className="flex flex-col items-center justify-center h-screen text-center p-4">
+      <FiAlertTriangle className="text-7xl text-black mb-4" />
 
-export default NotFound
+      <h1 className="text-6xl font-extrabold text-blac mb-4">404</h1>
+
+      <p className="text-2xl md:text-3xl font-light mb-8 text-gray-700">
+        Oops! The page you're looking for doesn't exist.
+      </p>
+
+      <Link
+        to="/"
+        className="px-8 py-4 bg-black text-white text-md font-medium rounded-full shadow-lg hover:bg-black hover:text-white transition-all duration-300 flex items-center"
+      >
+        <FiAlertTriangle className="mr-2" />
+        Go Back to Home
+      </Link>
+    </div>
+  );
+};
+
+export default NotFound;
